a6dc9219c98d03216aadcd5588983bf637a14cab
niyal jacob <niyaljacob76@gmail.com>
2024-06-19 17:28:23 +0530

    add some program to opp

--- fulldomain_dart/oop/class_object.dart
+++ fulldomain_dart/oop/class_object.dart
@@ -1,20 +1,24 @@
-class Area{
-  double? length;
-  double? breath;
+// class Area{
+//   double? length;
+//   double? breath;
   
-  Area({double? length=10, double? breath=20}){
-      this.length=length;
-      this.breath=breath;
-  } 
+//   Area({double? length=10, double? breath=20}){
+//       this.length=length;
+//       this.breath=breath;
+//   } 
 
-  double? findArea()
-  {
-    return length!*breath!;
-  }
-}
+//   double? findArea()
+//   {
+//     return length!*breath!;
+//   }
+// }
 
-void main(){
-  Area obj = Area();
+// void main(){
+//   Area obj = Area();
   
-  print("the area is ${obj.findArea()}");
-}
+//   print("the area is ${obj.findArea()}");
+// }
+
+
+// 
+
